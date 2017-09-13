c4c05ca714baa38e754781ab4d9e5fe001969ec5
hawaker <hawaker@qq.com>
2017-09-13 20:18:36 +0800

    create a admin with seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,20 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
+        DB::table('users')->insert([
+            'name' => "hawaker",
+            'email' => 'hawaker@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
+
 }
